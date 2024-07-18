2811172bb91e7e8aeed8f879e8eed8a7b1d56249
Greishia <150402306+Greishia@users.noreply.github.com>
2024-07-18 10:12:27 +0900

    Create SubjectListAction.java
    🤐

---
+++ tomcat/webapps/exam/WEB-INF/src/scoremanager/scoremanager.main/SubjectListAction.java
@@ -0,0 +1,32 @@
+package scoremanager.main;
+
+import java.time.LocalDate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import dao.SchoolDao;
+import java.util.*;
+
+import tool.Action;
+
+public class SubjectListAction extends Action{
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        HttpSession session = req.getSession();// セッション
+        Teacher teacher = (Teacher)session.getAttribute("user");
+
+        SubjectDao sDao = new SubjectDao();// 科目Dao
+
+        // DBからデータ取得
+        // ログインユーザーの学校コードをもとに科目コードの一覧を取得
+
+        List<Subject> subjects = sDao.filter(teacher.getSchool());
+
+        req.setAttribute("subjects", subjects);
+
+        req.getRequestDispatcher("subject_list.jsp").forward(req, res);
+    }
+}
