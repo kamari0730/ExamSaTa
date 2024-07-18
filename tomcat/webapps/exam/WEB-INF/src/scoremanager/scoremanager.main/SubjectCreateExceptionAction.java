ea8f338a3338339c3386a3d63635c68ab3add4a3
Greishia <150402306+Greishia@users.noreply.github.com>
2024-07-18 10:10:34 +0900

    Create SubjectCreateExceptionAction.java

---
+++ tomcat/webapps/exam/WEB-INF/src/scoremanager/scoremanager.main/SubjectCreateExceptionAction.java
@@ -0,0 +1,54 @@
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+import java.util.*;
+
+public class SubjectCreateExceptionAction extends Action{
+
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception{
+        HttpSession session = req.getSession();
+        Teacher teacher = (Teacher)session.getAttribute("user");
+        SubjectDao sDao = new SubjectDao();// 科目Dao
+        Map<String, String> errors = new HashMap<>();// エラーメッセージ
+
+        String cd = req.getParameter("cd");
+        String name = req.getParameter("name");
+        errors.get(cd);
+
+        Subject subjects = sDao.get(cd, teacher.getSchool());
+
+        if (cd.length() != 3) {
+            req.setAttribute("cd", cd);
+            req.setAttribute("name", name);
+            // System.out.println("三文字で入力");
+            errors.put("errors", "科目コードは3文字で入力してください。");
+            req.setAttribute("errors", errors);
+            req.getRequestDispatcher("subject_create.jsp").forward(req, res);
+
+        } else if (subjects != null) {
+            req.setAttribute("cd", cd);
+            req.setAttribute("name", name);
+            // System.out.println("値が重複");
+            errors.put("errors", "科目コードが重複しています。");
+            req.setAttribute("errors", errors);
+            req.getRequestDispatcher("subject_create.jsp").forward(req, res);
+
+        } else {
+            // System.out.println("異常なし");
+            Subject sub = new Subject();
+            sub.setCd(cd);
+            sub.setName(name);
+            sub.setSchool(((Teacher)session.getAttribute("user")).getSchool());
+            boolean flag = sDao.save(sub);
+            req.getRequestDispatcher("subject_create_done.jsp").forward(req, res);
+        }
+        
+    }
+    
+}
