9803114d1b70d1f6d38f3ea7f2cb9a85ef07fcd3
Greishia <150402306+Greishia@users.noreply.github.com>
2024-07-25 11:15:52 +0900

    Update SubjectDeleteExceptionAction.java
    😘

--- tomcat/webapps/exam/WEB-INF/src/scoremanager/scoremanager.main/SubjectDeleteExceptionAction.java
+++ tomcat/webapps/exam/WEB-INF/src/scoremanager/scoremanager.main/SubjectDeleteExceptionAction.java
@@ -10,22 +10,19 @@ import tool.Action;
 
 public class SubjectDeleteExceptionAction extends Action{
 
+    // cdからDBを特定し、削除する
     public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception{
         HttpSession session = req.getSession();
         Teacher teacher = (Teacher)session.getAttribute("user");
-        System.out.println("aaaa");
         
         String cd = req.getParameter("cd");
 
         Subject sub = new Subject();
         sub.setCd(cd);
-        System.out.println(cd);
         sub.setSchool(((Teacher)session.getAttribute("user")).getSchool());
         SubjectDao sDao = new SubjectDao();
-        boolean flag = sDao.delete(sub);
+        boolean flag = sDao.delete(sub);// DBから削除
 
         req.getRequestDispatcher("subject_delete_done.jsp").forward(req, res);
-        
     }
-    
 }
